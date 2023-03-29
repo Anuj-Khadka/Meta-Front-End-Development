7ba7090163d64f48d69a33d33fce95b45a522130
Anuj-Khadka <anujkhadka008@gmail.com>
2023-03-29 23:09:36 +0545

    update app

--- 6. Advanced React/advanced-react/src/App.js
+++ 6. Advanced React/advanced-react/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 
 // week one
 import ExerciseOneApp from './Week 1/Exercise 1 - Create a basic List component/src/ExerciseOneApp';
-
+import ExerciseFourApp from './Week 2/Exercise 4 - Managing state within a component/src/ExerciseFourApp';
 
 function App() {
   return (
     <div className="App">
       {/* Week 1 */}
-      <ExerciseOneApp />
+      {/* <ExerciseOneApp /> */}
+
+      {/* Week 2 */}
+      <ExerciseFourApp />
     </div>
   );
 }
