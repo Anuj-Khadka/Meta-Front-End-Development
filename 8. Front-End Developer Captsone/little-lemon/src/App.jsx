92e95270517d184727091ea937ec6913be0d0b63
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-17 23:21:07 +0545

    update little lemon

--- 8. Front-End Developer Captsone/little-lemon/src/App.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Highlights from './components/Highlights'
 
 function App() {
   return(
     <>
     <Header />
     <Hero />
-    {/* <Hero /> */}
+    <Highlights />
     <Footer />
     </>
   )
