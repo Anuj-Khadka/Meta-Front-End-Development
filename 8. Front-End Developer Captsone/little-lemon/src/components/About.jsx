1136e23bc789bccd364a149ac3487748196f5dd6
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-18 22:35:13 +0545

    update about section

--- 8. Front-End Developer Captsone/little-lemon/src/components/About.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/components/About.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className="about-section section">
+      <div className="about-content">
+        <h3 className="about-title">Little Lemon</h3>
+        <p className="about-subtitle">Chicago</p>
+        <p className="about-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
+          voluptatem quam error vitae soluta labore.
+        </p>
+      </div>
+      <div className="about-img-container"></div>
+      About
+    </section>
+  );
+};
 
-export default About
+export default About;
