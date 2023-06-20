e8c89816cd15da49825f5ef285a5d5debb81101e
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-20 23:27:29 +0545

    add booking feature

--- 8. Front-End Developer Captsone/little-lemon/src/App.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import About from './components/About'
+import BookingForm from './components/BookingForm'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
@@ -15,6 +16,20 @@ function App() {
     <Testimonials />
     <About />
     <Footer />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <hr />
+    <BookingForm />
     </>
   )
 }
