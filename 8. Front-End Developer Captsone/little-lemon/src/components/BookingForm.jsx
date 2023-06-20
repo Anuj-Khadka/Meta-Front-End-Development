5d5255c923f9497c77bd4a6683be0cbb32798dc0
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-21 01:20:54 +0545

    update booking form

--- 8. Front-End Developer Captsone/little-lemon/src/components/BookingForm.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/components/BookingForm.jsx
@@ -1,59 +1,81 @@
 import React, { useState } from "react";
+import "../assets/css/BookingForm.css"
 
 const BookingForm = () => {
-  const [date, setDate] = useState();
-  const [time, setTime] = useState();
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("17:00");
   const [noGuests, setNoGuests] = useState("1");
-  const [occasion, setOccasion] = useState("");
+  const [occasion, setOccasion] = useState("Birthday");
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleTimeChange = (e) => {
+    setTime(e.target.value);
+  };
+
+  const handleGuestsChange = (e) => {
+    setNoGuests(e.target.value);
+  };
+
+  const handleOccasionChange = (e) => {
+    setOccasion(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform form submission logic here
+  };
 
   return (
-    <>
-      <form
-        style={{
-          display: "grid",
-          maxWidth: "200px",
-          gap: "20px",
-          padding: "100px 200px",
-        }}
-      >
-        <label htmlFor="res-date">Choose date</label>
-        <input
-          type="date"
-          id="res-date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <label htmlFor="res-time">Choose time</label>
-        <select
-          id="res-time "
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        >
-          <option>17:00</option>
-          <option>18:00</option>
-          <option>19:00</option>
-          <option>20:00</option>
-          <option>21:00</option>
-          <option>22:00</option>
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-          value={noGuests}
-          onChange={(e) => setNoGuests(e.target.value)}
-        />
-        <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation" />
-      </form>
-    </>
+    <form
+    className="booking-form"
+      style={{
+        display: "grid",
+        maxWidth: "200px",
+        gap: "20px",
+        padding: "100px 200px",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={handleDateChange}
+      />
+
+      <label htmlFor="res-time">Choose time</label>
+      <select id="res-time" value={time} onChange={handleTimeChange}>
+        <option>17:00</option>
+        <option>18:00</option>
+        <option>19:00</option>
+        <option>20:00</option>
+        <option>21:00</option>
+        <option>22:00</option>
+      </select>
+
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={noGuests}
+        onChange={handleGuestsChange}
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select id="occasion" value={occasion} onChange={handleOccasionChange}>
+        <option>Birthday</option>
+        <option>Anniversary</option>
+      </select>
+
+      <input type="submit" value="Make Your reservation" />
+    </form>
   );
 };
 
