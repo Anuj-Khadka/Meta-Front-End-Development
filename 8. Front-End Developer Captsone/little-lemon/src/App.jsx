643a40417f943a53a9b1cf42378dea07231b0d73
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-21 23:28:54 +0545

    update routing

--- 8. Front-End Developer Captsone/little-lemon/src/App.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/App.jsx
@@ -1,36 +1,29 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import BookingForm from './components/BookingForm'
+import BookingPage from './components/BookingPage'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
 import Testimonials from './components/Testimonials'
+import Home from './components/Home'
 
 function App() {
   return(
-    <>
+    <BrowserRouter>
     <Header />
-    <Hero />
-    <Highlights />
-    <Testimonials />
-    <About />
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/menu" element={<Highlights />}/>
+      <Route path="/" element={<Testimonials />}/>
+      <Route path="/about" element={<About />}/>
+      <Route path="/reservations" element={<BookingPage />}/>
+    </Routes>
+    
     <Footer />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <hr />
-    <BookingForm />
-    </>
+    </BrowserRouter>
   )
 }
 
