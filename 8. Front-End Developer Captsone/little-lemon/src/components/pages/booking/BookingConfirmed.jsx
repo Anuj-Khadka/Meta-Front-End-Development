9c7d58b5573229d01cfbddf22e2c16f9ab880041
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-23 00:30:39 +0545

    update booking page

--- 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingConfirmed.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingConfirmed.jsx
@@ -1,11 +1,28 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 const BookingConfirmed = () => {
-    return (
-        <div className="booking-confirmed">
-         <p className="title confirmed-title">Booking Confirmed!!!</p>
-         <p className="subtitle confirmed-subtitle">Please provide your name and contact number at the reception upon arrival</p>
-         <p className="subtitle confirmed-subtitle">We are excited to serve you</p>
-        </div>
-      )
+  const navigate = useNavigate();
+
+  const handleReturn = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="booking-confirmed">
+      <h3 className="title confirmed-title">Booking Confirmed!!!</h3>
+      <p className="subtitle confirmed-subtitle">
+        Please provide your name and contact number at the reception upon
+        arrival.
+      </p>
+      <p className="subtitle confirmed-subtitle">
+        We are excited to serve you! ;)
+      </p>
+      <button className="return-res" onClick={handleReturn}>
+        Go To Home
+      </button>
+    </div>
+  );
 };
 
 export default BookingConfirmed;
