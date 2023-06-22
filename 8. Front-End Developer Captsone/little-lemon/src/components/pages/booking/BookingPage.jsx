ee72012d8c03f31ef52da4c0c638e0e046b0e8e5
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-22 23:10:30 +0545

    update booking page

--- 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingPage.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingPage.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import BookingForm from "./BookingForm"
-import "../../../assets/css/BookingForm.css"
+import { useState, useReducer } from "react";
+import BookingForm from "./BookingForm";
+import "../../../assets/css/BookingForm.css";
+import { useNavigate } from "react-router-dom";
+import { fetchAPI, submitAPI } from "../../../apis/api";
 
 const BookingPage = () => {
-  return (
-    <BookingForm />
-  )
-}
+  const [date, setDate] = useState(new Date());
 
-export default BookingPage
+  function initializeTimes(date) {
+    return fetchAPI(date);
+  }
+
+  function updateTimes(date) {
+    const dateObj = new Date(date);
+    return fetchAPI(dateObj);
+  }
+
+  const navigate = useNavigate();
+
+  function submitForm(formData) {
+    const isSubmitted = submitAPI(formData);
+
+    if (isSubmitted) {
+      navigate("/confirmed");
+    }
+  }
+
+  function reducer(state, action) {
+    let newState;
+    switch (action.type) {
+      case "UPDATE_TIMES":
+        const newDate = new Date(action.payload);
+        newState = updateTimes(newDate);
+        break;
+
+      default:
+        throw new Error();
+    }
+    return newState;
+  }
+
+  const [availableTimes, dispatch] = useReducer(reducer, initializeTimes(date));
+  return (
+    <BookingForm
+      availableTimes={availableTimes}
+      dispatch={dispatch}
+      submitForm={submitForm}
+    />
+  );
+};
+
+export default BookingPage;
