5c90c5d59d066b00520a45161008f3e6872a5a6d
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-22 21:53:49 +0545

    restructured files

---
+++ 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import BookingForm from "./BookingForm"
+import "../../../assets/css/BookingForm.css"
+
+const BookingPage = () => {
+  return (
+    <BookingForm />
+  )
+}
+
+export default BookingPage
