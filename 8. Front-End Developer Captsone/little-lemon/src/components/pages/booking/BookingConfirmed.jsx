d7fb99827117bde8958026936379b4465e597ec1
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-22 23:09:55 +0545

    update booking page

---
+++ 8. Front-End Developer Captsone/little-lemon/src/components/pages/booking/BookingConfirmed.jsx
@@ -0,0 +1,11 @@
+const BookingConfirmed = () => {
+    return (
+        <div className="booking-confirmed">
+         <p className="title confirmed-title">Booking Confirmed!!!</p>
+         <p className="subtitle confirmed-subtitle">Please provide your name and contact number at the reception upon arrival</p>
+         <p className="subtitle confirmed-subtitle">We are excited to serve you</p>
+        </div>
+      )
+};
+
+export default BookingConfirmed;
