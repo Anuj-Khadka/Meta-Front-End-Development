1860e7898f83e8ce2f3bb83dac3b8774215ad8d7
Anuj-Khadka <anujkhadka008@gmail.com>
2023-06-22 23:10:22 +0545

    update booking page

--- 8. Front-End Developer Captsone/little-lemon/src/App.jsx
+++ 8. Front-End Developer Captsone/little-lemon/src/App.jsx
@@ -1,28 +1,30 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import About from './components/pages/home/About'
-import BookingPage from './components/pages/booking/BookingPage'
-import Footer from './components/layout/Footer'
-import Header from './components/layout/Header'
-import Highlights from './components/pages/home/Highlights'
-import Testimonials from './components/pages/home/Testimonials'
-import Home from './components/pages/home/Home'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/pages/home/About";
+import BookingPage from "./components/pages/booking/BookingPage";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Highlights from "./components/pages/home/Highlights";
+import Testimonials from "./components/pages/home/Testimonials";
+import Home from "./components/pages/home/Home";
+import PageNotFound from "./components/layout/PageNotFound";
 
 function App() {
-  return(
+  return (
     <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/menu" element={<Highlights />}/>
-      <Route path="/" element={<Testimonials />}/>
-      <Route path="/about" element={<About />}/>
-      <Route path="/reservations" element={<BookingPage />}/>
-    </Routes>
-    
-    <Footer />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Highlights />} />
+        <Route path="/" element={<Testimonials />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/reservations" element={<BookingPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
